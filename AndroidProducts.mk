3dc8ace106bc3bd71fa734922712ea909a035c94
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-18 22:12:53 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_init_boot.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_init_boot-user \
+    omni_init_boot-userdebug \
+    omni_init_boot-eng
+
---
+++ omni_init_boot.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from init_boot device
+$(call inherit-product, device/generic/init_boot/device.mk)
+
+PRODUCT_DEVICE := init_boot
+PRODUCT_NAME := omni_init_boot
+PRODUCT_BRAND := generic
+PRODUCT_MODEL := Generic Device
+PRODUCT_MANUFACTURER := generic
+
+PRODUCT_GMS_CLIENTID_BASE := android-generic
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC=""
+
+BUILD_FINGERPRINT := 
+
