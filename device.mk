7260d927317e77c7b33fe1186b7ee6710250e4e7
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-18 22:22:07 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-LOCAL_PATH := device/generic/init_boot
+LOCAL_PATH := device/xiaomi/dew
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
