8ac326980db25c57c4a0aedfd7b61dbc75da0cb5
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-19 14:03:18 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,7 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fox_dew.mk
+    $(LOCAL_DIR)/fox_dew.mk \
+    $(LOCAL_DIR)/twrp_dew.mk
 
 COMMON_LUNCH_CHOICES := \
     omni_dew-user \
