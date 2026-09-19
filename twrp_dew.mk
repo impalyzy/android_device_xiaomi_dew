c0d3227e95bc049e487b9dafa5dab464b9185853
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-19 14:15:00 +0800

    Update copyright and SPDX license in twrp_dew.mk

--- twrp_dew.mk
+++ twrp_dew.mk
@@ -1,3 +1,9 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 # Inherit from core products
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
