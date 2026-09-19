7d7ebf327d5ffae538bd91423af7939b9ef01800
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-19 11:00:22 +0800

    Update omni_dew.mk for AOSP and TWRP integration

--- omni_dew.mk
+++ omni_dew.mk
@@ -3,16 +3,18 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from core products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit TWRP common configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from dew device
 $(call inherit-product, device/xiaomi/dew/device.mk)
 
+# Product Identifiers
 PRODUCT_DEVICE := dew
 PRODUCT_NAME := omni_dew
 PRODUCT_BRAND := Xiaomi
