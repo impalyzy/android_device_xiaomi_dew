049a627d7b94a26f3dae3fb17fb16f64a6597d1b
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-19 10:41:56 +0800

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo omni_init_boot-user
-add_lunch_combo omni_init_boot-userdebug
-add_lunch_combo omni_init_boot-eng
-
